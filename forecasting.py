b24175e0b4dace4862c69b37e40f4a6bc41e8b51
convergytics12 <131863819+convergytics12@users.noreply.github.com>
2023-05-09 16:21:41 +0530

    Update forecasting.py

--- forecasting.py
+++ forecasting.py
@@ -309,6 +309,89 @@ if file1 is not None:
                     dff.to_csv(str(sb)+'_forecasted_data.csv')
                 if rad=='.xlsx':
                     dff.to_excel(str(sb)+'_forecasted_data.xlsx')
+    if(sb==5007):
+        x=finaldf[finaldf['Plant']==sb][['2020-01','2020-02','2020-03','2020-04','2020-05','2020-06','2020-07','2020-08','2020-09','2020-10','2020-11','2020-12','2021-01','2021-02','2021-03','2021-04','2021-05','2021-06','2021-07','2021-08','2021-09','2021-10','2021-11','2021-12','2022-01','2022-02','2022-03','2022-04','2022-05','2022-06','2022-07','2022-08','2022-09','2022-10','2022-11','2022-12']]
+        dd=pd.DataFrame(x.sum())
+        dd=dd.reset_index()
+        dd.rename(columns={'index':'Year_Month',0:'Production'},inplace=True)
+        dd=dd.set_index('Year_Month')
+        dd.index=pd.to_datetime(dd.index)
+        finaldf=dd
+        st.subheader('Information Provided')
+        st.dataframe(finaldf)
+        st.line_chart(finaldf)
+        df=finaldf
+             
+        
+        
+        
+        st.subheader('Seasonal Decompose')
+        result = seasonal_decompose(x=df['Production'], model='multiplicative',)
+
+        fig = make_subplots(
+            rows=4, cols=1,
+            subplot_titles=["Observed", "Trend", "Seasonal", "Residuals"]
+        )
+        
+        fig.add_trace(
+            go.Scatter(x=result.seasonal.index, y=result.observed, mode='lines', name="Observed"),
+            row=1, col=1
+        )
+        
+        fig.add_trace(
+            go.Scatter(x=result.trend.index, y=result.trend, mode='lines', name="Trend"),
+            row=2, col=1
+        )
+        
+        fig.add_trace(
+            go.Scatter(x=result.seasonal.index, y=result.seasonal, mode='lines', name="Seasonal"),
+            row=3, col=1
+        )
+        
+        fig.add_trace(
+            go.Scatter(x=result.resid.index, y=result.resid, mode='lines', name="Residuals"),
+            row=4, col=1
+        )
+        
+        fig.update_layout(height=600, width=1000)
+        st.plotly_chart(fig, use_container_width=True)
+        
+    
+        st.subheader('Actual Production Vs Predicted Production')
+        model = sm.tsa.arima.ARIMA(df['Production'], order=(2,1,1))  
+        model1 = model.fit()
+        pred = model1.predict()
+        df['prediction'] = pred.values
+        df.reset_index(inplace=True)
+        df['prediction'] = df['prediction'].shift(periods=-1)
+        df['prediction'] = df['prediction'].replace(np.nan, 0)
+        
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=df['Year_Month'], y=df['Production'], name='Actual Production', line=dict(color='blue')))
+        fig.add_trace(go.Scatter(x=df['Year_Month'], y=df['prediction'], name='Predicted Production', line=dict(color='orange')))
+        st.plotly_chart(fig)
+        
+        mape1 = mape(df['Production'], df['prediction'])
+   
+        st.write('MAPE:',mape1)
+        fore=model1.forecast(steps=6)
+        dff=pd.DataFrame(data=list(fore.values),columns=['Forecasted Production'],index=pd.Series((fore.index.values)))
+        dff=dff.reset_index().rename(columns={'index':'Date'})
+        
+        if(st.button('Start Forecasting')):
+            st.subheader('Forecasted Production')
+            fig = go.Figure()
+            #fig.add_trace(go.Scatter(x=df['Year_Month'], y=df['Production'], name='Actual Production', line=dict(color='blue')))
+            fig.add_trace(go.Scatter(x=dff['Date'], y=dff['Forecasted Production'], name='Forecasted Production', line=dict(color='orange')))
+            st.plotly_chart(fig)
+            
+       
+            rad=st.radio('**Export Forecasted Production**',['.csv','.xlsx'])
+            if(st.button('Download')):
+                if rad=='.csv':
+                    dff.to_csv(str(sb)+'_forecasted_data.csv')
+                if rad=='.xlsx':
+                    dff.to_excel(str(sb)+'_forecasted_data.xlsx')
                     
     if(sb==5008):
         x=finaldf[finaldf['Plant']==sb][['2020-01','2020-02','2020-03','2020-04','2020-05','2020-06','2020-07','2020-08','2020-09','2020-10','2020-11','2020-12','2021-01','2021-02','2021-03','2021-04','2021-05','2021-06','2021-07','2021-08','2021-09','2021-10','2021-11','2021-12','2022-01','2022-02','2022-03','2022-04','2022-05','2022-06','2022-07','2022-08','2022-09','2022-10','2022-11','2022-12']]
